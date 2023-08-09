ca9885673ebad08a5e33c3f7c3580663246c8565
RafaelAdrianMejia <tucsonrafa@outlook.com>
2023-08-08 18:36:45 -0700

    exercise completed

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,7 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLot = new Carlot();
             //TODO
 
             //Create a seperate class file called Car
@@ -18,9 +19,37 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            //Dot Notation
+            var charger = new Car();
+            charger.Make = "Dodge";
+            charger.Model = "Challenger";
+            charger.Year = 1970;
+            charger.IsDriveable = true;
+            charger.EngineNoise = "smooth";
+            charger.HonkNoise = "honk";
+
+            carLot.ParkingLot.Add(charger);
+
+
+            //Object initializer syntax
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Silverado",
+                Year = 2023,
+                IsDriveable = true,
+                EngineNoise = "vroom",
+                HonkNoise = "beep"
+            };
+
+            carLot.ParkingLot.Add(chevy);
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            var sierra = new Car(2023, "GMC", "Sierra", "click clack", "HONK!", true);
+            
+            carLot.ParkingLot.Add(sierra);
 
             //*************BONUS X 2*************//
 
@@ -28,6 +57,8 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            carLot.CheckCars();
         }
     }
 }
